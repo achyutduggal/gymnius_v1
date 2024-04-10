220276e36b5a6f9822cfb4112d36aedfffccbb21
achyu <achyut1234duggal@gmail.com>
2024-04-10 10:38:07 +0530

    Added drinking_screen.dart

--- lib/Screens/drinking_screen.dart
+++ lib/Screens/drinking_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
-class DrinkingScreen extends StatelessWidget {
-  const DrinkingScreen({super.key});
+class WaterIntakeScreen extends StatefulWidget {
+  const WaterIntakeScreen({super.key});
+
+  @override
+  State<WaterIntakeScreen> createState() => _WaterIntakeScreenState();
+}
+
+class _WaterIntakeScreenState extends State<WaterIntakeScreen> {
+  int _waterIntake = 0;
+
+  void _incrementWaterIntake() {
+    setState(() {
+      _waterIntake += 250;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have consumed $_waterIntake ml of water today.',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _incrementWaterIntake,
+              child: const Text('Add 250ml'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
